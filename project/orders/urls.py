4c906a83448109262e15f6fcbda8e2e4d131a6f1
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-11-24 17:14:34 -0500

    :zap: Added request url

--- project/orders/urls.py
+++ project/orders/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import OrderListView
+from .views import OrderListView, get_data
 
 app_name = "orders"
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="list"),
+    path("request/", get_data, name="get_data")
 ]
