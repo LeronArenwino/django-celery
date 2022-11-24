f8e577fdb29234f3036e5d56e45d5a8706696b57
Francisco Dueñas <frsduenasca@unal.edu.co>
2022-11-24 17:14:46 -0500

    :zap: New view

--- project/orders/views.py
+++ project/orders/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 
-from .models import Order, Product
+import logging
+import requests
 
+from .models import Order
+
+logger = logging.getLogger(__name__)
 
 class OrderListView(ListView):
     model = Order
+
+def get_data(request):
+
+    url = 'http://universities.hipolabs.com/search'
+
+    response = requests.get(url=url)
+    data = response.json()
+    print(data)
+
+    return render(request, 'order_request.html', {'response': data})
